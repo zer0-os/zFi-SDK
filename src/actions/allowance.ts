028baf77edb96a25482648c13fcc8b174791ecbb
James Earle <james@zero.tech>
2021-12-15 16:44:12 -0500

    address comments and add return to abi

--- src/actions/allowance.ts
+++ src/actions/allowance.ts
@@ -3,14 +3,14 @@ import { getCorePool } from "../helpers";
 import { SubConfig } from "../types";
 
 // Add ERC20 `allowance`
-const erc20Abi = ["function allowance(address _owner, address _spender) public"];
+const erc20Abi = ["function allowance(address _owner, address _spender) public view returns (uint256)"];
 
-export const allowance = async (signer: ethers.Signer, config: SubConfig): Promise<ethers.ContractTransaction> => {
+export const allowance = async (signer: ethers.Signer, config: SubConfig): Promise<ethers.BigNumber> => {
   const pool = await getCorePool(config);
   const poolToken = await pool.poolToken();
   const signerAddress = await signer.getAddress();
 
   const tokenInstance = new ethers.Contract(poolToken, erc20Abi, config.provider);
-  const tx = await tokenInstance.allowance(signerAddress, config.address);
-  return tx;
+  const allowance = await tokenInstance.allowance(signerAddress, config.address);
+  return allowance;
 }
